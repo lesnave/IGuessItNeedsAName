07361d0f541afc6d43af60744a296d61fbb1d77a
lesnave <eggt1991@gmail.com>
2017-05-26 19:48:16 -0500

    Yo angular build

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name iguessItNeedsAnameApp
+ * @description
+ * # iguessItNeedsAnameApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('iguessItNeedsAnameApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iguessItNeedsAnameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the iguessItNeedsAnameApp
+ */
+angular.module('iguessItNeedsAnameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
